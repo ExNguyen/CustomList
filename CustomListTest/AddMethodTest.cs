19418e63226dd6876482e9574206fc4863787a9b
Ex <exeone.nguyen@gmail.com>
2023-10-19 05:00:24 -0400

    Update AddMethodTest.cs
    Completed all AddMethod's TestMethod. Each test method passed.

--- CustomListTest/AddMethodTest.cs
+++ CustomListTest/AddMethodTest.cs
@@ -52,8 +52,41 @@ public class AddMethodTest
     [TestMethod]
     public void AddMethod_CapacityIncrease_CapacityDoubleWhenExceeded()
     {
+        //Arrange
+        CustomList<int> myList = new CustomList<int>();
 
+        //Act
+        myList.Add(1);
+        myList.Add(2);
+        myList.Add(3);
+        myList.Add(4);
+        myList.Add(5);
+
+        //Assert
+        Assert.AreEqual(8, myList.Capacity);
     }
 
-    
+    [TestMethod]
+    public void AddMethod_CapacityArrayAfterIncrease_OriginalNumbersInSameIndex()
+    {
+        //Arrange
+        CustomList<int> myList = new CustomList<int>();
+
+        //Act
+        myList.Add(1);
+        myList.Add(2);
+        myList.Add(3);
+        myList.Add(4);
+        myList.Add(5);
+
+        //Assert
+        Assert.AreEqual(1, myList[0]);
+        Assert.AreEqual(2, myList[1]);
+        Assert.AreEqual(3, myList[2]);
+        Assert.AreEqual(4, myList[3]);
+        Assert.AreEqual(5, myList[4]);
+        
+    }
+
+
 }
