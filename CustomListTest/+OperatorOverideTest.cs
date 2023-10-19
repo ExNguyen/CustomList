9811360828c6ccd8f92b500e079051d8ada96939
Ex <exeone.nguyen@gmail.com>
2023-10-19 06:13:59 -0400

    Created PlusOperator TestMethod
    Began writing first TestMethod, incomplete.

---
+++ CustomListTest/+OperatorOverideTest.cs
@@ -0,0 +1,23 @@
+﻿using CustomList;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class PlusOperatorOverideTest
+    {
+        [TestMethod]
+        public void PlusOperatorOverideTest_ListLength_FirstListLongerThenSecondList()
+        {
+            //Arrange
+            CustomList<int> list1 = new CustomList<int>();
+            CustomList<int> list2 = new CustomList<int>();
+
+            //Act
+            list1.Add(1);
+
+            //Assert
+            
+
+        }
+    }
+}
