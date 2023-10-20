84e7bd1125973cddb67e55967067a44b922232ba
Ex <exeone.nguyen@gmail.com>
2023-10-20 03:04:33 -0400

    Operator Override Test methods
    completed plus operator override test method, still untested

---
+++ CustomListTest/MinusOperatorOverideTest.cs
@@ -0,0 +1,20 @@
+﻿using CustomList;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class MinusOperatorOverideTest
+    {
+        [TestMethod]
+        public void MinusOperatorMethod()
+        {
+            //Arrange
+            CustomList<int> mylist = new CustomList<int>();
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
---
+++ CustomListTest/PlusOperatorOverrideTest.cs
@@ -0,0 +1,74 @@
+﻿using CustomList;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class PlusOperatorOverideTest
+    {
+        [TestMethod]
+        public void PlusOperatorMethod_ListLength_FirstListLongerThenSecondList()
+        {
+            //Arrange
+            CustomList<int> list1 = new CustomList<int>();
+            CustomList<int> list2 = new CustomList<int>();
+
+            //Act
+            list1.Add(1);
+            list1.Add(2);
+            list1.Add(3);
+
+            list2.Add(4);
+            list2.Add(5);
+
+            CustomList<int> combinedList = list1 + list2;
+
+            //Assert
+            Assert.AreEqual(5, combinedList.Count);
+            Assert.AreEqual(1, combinedList[0]);
+            Assert.AreEqual(5, combinedList[4]);
+
+        }
+
+        [TestMethod]
+        public void PlusOperatorMethod_ListLength_SecondListLongerThenFirstList()
+        {
+            //Arrange
+            CustomList<int> list1 = new CustomList<int>();
+            CustomList<int> list2 = new CustomList<int>();
+
+            //Act
+            list1.Add(1);
+            list1.Add(2);
+
+            list2.Add(3);
+            list2.Add(4);
+            list2.Add(5);
+
+            CustomList<int> combinedList = list1 + list2;
+
+            //Assert
+            Assert.AreEqual(5, combinedList.Count);
+            Assert.AreEqual(1, combinedList[0]);
+            Assert.AreEqual(5, combinedList[4]);
+        }
+
+        [TestMethod]
+        public void PlusOperatorMethod_OneListEmpty_NewListHasCorrectItems()
+        {
+            //Arrange
+            CustomList<int> list1 = new CustomList<int>();
+            CustomList<int> list2 = new CustomList<int>();
+
+            //Act
+            list1.Add(1);
+            list1.Add(2)
+
+            CustomList<int> combinedList = list1 + list2;
+
+            //Assert
+            Assert.AreEqual(2, combinedList.Count);
+            Assert.AreEqual(1, combinedList[0]);
+            Assert.AreEqual("", combinedList[3]);
+        }
+    }
+}
